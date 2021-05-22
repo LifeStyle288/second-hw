05aa7aaef6a37a2953eb0315ab2d9b17926f619b
g.severgin <g.severgin@prosoftsystems.ru>
2021-05-22 17:38:49 +0500

    Сб 22 мая 2021 17:38:49 +05

--- tests.cpp
+++ tests.cpp
@@ -14,7 +14,7 @@ BOOST_AUTO_TEST_SUITE(tests)
 
 BOOST_AUTO_TEST_CASE(test_map_allocate)
 {
-    BOOST_MESSAGE("Start test_map_allocate...");
+    BOOST_TEST_MESSAGE("Start test_map_allocate...");
     using TestMap = std::map<int, int, std::less<int>, MyAllocatorMap<int, int, MAX_ELEMENTS>>;
     TestMap m;
     for (size_t i = 0; i < MAX_ELEMENTS; ++i)
@@ -28,14 +28,14 @@ BOOST_AUTO_TEST_CASE(test_map_allocate)
     {
         BOOST_CHECK(*it1 == *it2);
     }
-    BOOST_MESSAGE("Checks true...");
+    BOOST_TEST_MESSAGE("Checks true...");
     BOOST_CHECK_THROW(m.insert({10, factorial(10)}), std::bad_alloc);
-    BOOST_MESSAGE("Stop test_map_allocate...");
+    BOOST_TEST_MESSAGE("Stop test_map_allocate...");
 }
 
 BOOST_AUTO_TEST_CASE(test_vector_allocate)
 {
-    BOOST_MESSAGE("Start test_vector_allocate...");
+    BOOST_TEST_MESSAGE("Start test_vector_allocate...");
     using TestVec = std::vector<int, MyAllocatorVec<int, MAX_ELEMENTS>>;
     TestVec v;
     for (int i = 0; i < MAX_ELEMENTS; ++i)
@@ -50,12 +50,12 @@ BOOST_AUTO_TEST_CASE(test_vector_allocate)
             v.push_back(i);
         }
     }
-    BOOST_MESSAGE("Stop test_vector_allocate...");
+    BOOST_TEST_MESSAGE("Stop test_vector_allocate...");
 }
 
 BOOST_AUTO_TEST_CASE(test_my_vector_allocate)
 {
-    BOOST_MESSAGE("Start test_my_vector_allocate...");
+    BOOST_TEST_MESSAGE("Start test_my_vector_allocate...");
     using TestVec = SimpleVector<int, MyAllocatorVec<int, MAX_ELEMENTS>>;
     TestVec v(MAX_ELEMENTS);
     for (int i = 0; i < MAX_ELEMENTS; ++i)
@@ -64,9 +64,9 @@ BOOST_AUTO_TEST_CASE(test_my_vector_allocate)
     }
     const TestVec sample = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
     BOOST_CHECK_EQUAL_COLLECTIONS(v.begin(), v.end(), sample.begin(), sample.end());
-    BOOST_MESSAGE("Check equal collections true...")
+    BOOST_TEST_MESSAGE("Check equal collections true...")
     BOOST_CHECK_THROW(v.push_back(111), std::bad_alloc);
-    BOOST_MESSAGE("Stop test_my_vector_allocate...");
+    BOOST_TEST_MESSAGE("Stop test_my_vector_allocate...");
 }
 
 BOOST_AUTO_TEST_SUITE_END()
